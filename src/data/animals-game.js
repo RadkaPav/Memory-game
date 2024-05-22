d60f390e5f0062060a43f5fdbdfe2935b86a967a
Radka Pavlišová <radojka@seznam.cz>
2024-05-22 08:46:35 +0200

    Přidán soubor animals-game.js

---
+++ src/data/animals-game.js
@@ -0,0 +1,83 @@
+import image1 from "../images/sheep.png"
+import image2 from "../images/elephant.png"
+import image3 from "../images/giraffe.png"
+import image4 from "../images/panda.png"
+import image5 from "../images/squirrel.png"
+import image6 from "../images/whale.png"
+
+const animals = [
+    {
+        id: 1,
+        name: "sheep",
+        image: image1,
+        matched: false
+    },
+    {
+        id: 2,
+        name: "sheep",
+        image: image1,
+        matched: false
+    },
+    {
+        id: 3,
+        name: "elephant",
+        image: image2,
+        matched: false
+    },
+    {
+        id: 4,
+        name: "elephant",
+        image: image2,
+        matched: false
+    },
+    {
+        id: 5,
+        name: "giraffe",
+        image: image3,
+        matched: false
+    },
+    {
+        id: 6,
+        name: "giraffe",
+        image: image3,
+        matched: false
+    },
+    {
+        id: 7,
+        name: "panda",
+        image: image4,
+        matched: false
+    },
+    {
+        id: 8,
+        name: "panda",
+        image: image4,
+        matched: false
+    },
+    {
+        id: 9,
+        name: "squirrel",
+        image: image5,
+        matched: false
+    },
+    {
+        id: 10,
+        name: "squirrel",
+        image: image5,
+        matched: false
+    },
+    {
+        id: 11,
+        name: "whale",
+        image: image6,
+        matched: false
+    },
+    {
+        id: 12,
+        name: "whale",
+        image: image6,
+        matched: false
+    },
+]
+
+export default animals
