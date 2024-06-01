f730bbc5157512cca0453d42c5e9e768ef8d7a06
Radka Pavlišová <radojka@seznam.cz>
2024-06-01 15:32:22 +0200

    Upraven soubor animals-game.js

--- src/data/animals-game.js
+++ src/data/animals-game.js
@@ -4,98 +4,99 @@ import image2 from "../images-sounds/animals/elephant.png"
 import image4 from "../images-sounds/animals/panda.png"
 import image5 from "../images-sounds/animals/squirrel.png"
 import image6 from "../images-sounds/animals/whale.png"
+import image7 from "../images-sounds/animals/rabbit.png"
+import image8 from "../images-sounds/animals/fox.png"
+import image9 from "../images-sounds/animals/lion.png"
+import image10 from "../images-sounds/animals/tiger.png"
+import image11 from "../images-sounds/animals/koala.png"
+import image12 from "../images-sounds/animals/hippo.png"
+
 import audio1 from "../images-sounds/animals/sheep.mp3"
 import audio2 from "../images-sounds/animals/elephant.mp3"
 import audio3 from "../images-sounds/animals/giraffe.mp3"
 import audio4 from "../images-sounds/animals/panda.mp3"
 import audio5 from "../images-sounds/animals/squirrel.mp3"
 import audio6 from "../images-sounds/animals/whale.mp3"
+import audio7 from "../images-sounds/animals/rabbit.mp3"
+import audio8 from "../images-sounds/animals/fox.mp3"
+import audio9 from "../images-sounds/animals/lion.mp3"
+import audio10 from "../images-sounds/animals/tiger.mp3"
+import audio11 from "../images-sounds/animals/koala.mp3"
+import audio12 from "../images-sounds/animals/hippo.mp3"
 
 const animals = [
     {
-        id: 1,
         name: "sheep",
-        image: image1,
-        sound: audio1,
+        image: "image1",
+        sound: "audio1",
         matched: false
     },
     {
-        id: 2,
-        name: "sheep",
-        image: image1,
-        sound: audio1,
-        matched: false
-    },
-    {
-        id: 3,
         name: "elephant",
-        image: image2,
-        sound: audio2,
+        image: "image2",
+        sound: "audio2",
         matched: false
     },
     {
-        id: 4,
-        name: "elephant",
-        image: image2,
-        sound: audio2,
-        matched: false
-    },
-    {
-        id: 5,
         name: "giraffe",
-        image: image3,
-        sound: audio3,
+        image: "image3",
+        sound: "audio3",
         matched: false
     },
     {
-        id: 6,
-        name: "giraffe",
-        image: image3,
-        sound: audio3,
-        matched: false
-    },
-    {
-        id: 7,
         name: "panda",
-        image: image4,
-        sound: audio4,
+        image: "image4",
+        sound: "audio4",
         matched: false
     },
     {
-        id: 8,
-        name: "panda",
-        image: image4,
-        sound: audio4,
-        matched: false
-    },
-    {
-        id: 9,
         name: "squirrel",
-        image: image5,
-        sound: audio5,
+        image: "image5",
+        sound: "audio5",
         matched: false
     },
     {
-        id: 10,
-        name: "squirrel",
-        image: image5,
-        sound: audio5,
-        matched: false
-    },
-    {
-        id: 11,
         name: "whale",
-        image: image6,
-        sound: audio6,
+        image: "image6",
+        sound: "audio6",
         matched: false
     },
     {
-        id: 12,
-        name: "whale",
-        image: image6,
-        sound: audio6,
+        name: "rabbit",
+        image: "image7",
+        sound: "audio7",
         matched: false
     },
+    {
+        name: "fox",
+        image: "image8",
+        sound: "audio8",
+        matched: false
+    },
+    {
+        name: "lion",
+        image: "image9",
+        sound: "audio9",
+        matched: false
+    },
+    {
+        name: "tiger",
+        image: "image10",
+        sound: "audio10",
+        matched: false
+    },
+    {
+        name: "koala",
+        image: "image11",
+        sound: "audio11",
+        matched: false
+    },
+    {
+        name: "hippo",
+        image: "image12",
+        sound: "audio12",
+        matched: false
+    }
 ]
 
 export default animals
