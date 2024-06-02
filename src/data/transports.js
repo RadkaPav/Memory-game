bbf54daf6aa51516422b1f23423af74338b472f7
Radka Pavlišová <radojka@seznam.cz>
2024-06-02 13:16:49 +0200

    Přidán soubor transports.js

---
+++ src/data/transports.js
@@ -0,0 +1,110 @@
+import image1 from "../images-sounds/transports/car.png"
+import image2 from "../images-sounds/transports/bus.png"
+import image3 from "../images-sounds/transports/train.png"
+import image4 from "../images-sounds/transports/scooter.png"
+import image5 from "../images-sounds/transports/bike.png"
+import image6 from "../images-sounds/transports/boat.png"
+import image7 from "../images-sounds/transports/ship.png"
+import image8 from "../images-sounds/transports/plane.png"
+import image9 from "../images-sounds/transports/air-balloon.png"
+import image10 from "../images-sounds/transports/ambulance.png"
+import image11 from "../images-sounds/transports/lorry.png"
+import image12 from "../images-sounds/transports/helicopter.png"
+import image13 from "../images-sounds/transports/motorbike.png"
+
+import audio1 from "../images-sounds/transports/car.mp3"
+import audio2 from "../images-sounds/transports/bus.mp3"
+import audio3 from "../images-sounds/transports/train.mp3"
+import audio4 from "../images-sounds/transports/scooter.mp3"
+import audio5 from "../images-sounds/transports/bike.mp3"
+import audio6 from "../images-sounds/transports/boat.mp3"
+import audio7 from "../images-sounds/transports/ship.mp3"
+import audio8 from "../images-sounds/transports/plane.mp3"
+import audio9 from "../images-sounds/transports/air-balloon.mp3"
+import audio10 from "../images-sounds/transports/ambulance.mp3"
+import audio11 from "../images-sounds/transports/lorry.mp3"
+import audio12 from "../images-sounds/transports/helicopter.mp3"
+import audio13 from "../images-sounds/transports/motorbike.mp3"
+
+const transports = [
+    {
+        name: "car",
+        image: image1,
+        sound: audio1,
+        matched: false
+    },
+    {
+        name: "bus",
+        image: image2,
+        sound: audio2,
+        matched: false
+    },
+    {
+        name: "train",
+        image: image3,
+        sound: audio3,
+        matched: false
+    },
+    {
+        name: "scooter",
+        image: image4,
+        sound: audio4,
+        matched: false
+    },
+    {
+        name: "bike",
+        image: image5,
+        sound: audio5,
+        matched: false
+    },
+    {
+        name: "boat",
+        image: image6,
+        sound: audio6,
+        matched: false
+    },
+    {
+        name: "ship",
+        image: image7,
+        sound: audio7,
+        matched: false
+    },
+    {
+        name: "plane",
+        image: image8,
+        sound: audio8,
+        matched: false
+    },
+    {
+        name: "air balloon",
+        image: image9,
+        sound: audio9,
+        matched: false
+    },
+    {
+        name: "ambulance",
+        image: image10,
+        sound: audio10,
+        matched: false
+    },
+    {
+        name: "lorry",
+        image: image11,
+        sound: audio11,
+        matched: false
+    },
+    {
+        name: "helicopter",
+        image: image12,
+        sound: audio12,
+        matched: false
+    },
+    {
+        name: "motorbike",
+        image: image13,
+        sound: audio13,
+        matched: false
+    }
+]
+
+export default transports
