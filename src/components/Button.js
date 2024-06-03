7222e7d8abe0622608e9e8ddf5de8ed63cc20623
Radka Pavlišová <153281974+RadkaPav@users.noreply.github.com>
2024-06-03 15:12:13 +0200

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Button = ( {name, changeDifficulty, option, id} ) => {
     return (
-        <div className={`border-2 bg-green-800 px-3 py-1 rounded-md text-white
-                        ${id === option && "bg-[#83ba2e]"}
-        `} onClick={() => changeDifficulty(id)}>
+        <div className={`border-2 px-3 py-1 rounded-md text-white cursor-pointer
+                        ${id === option ? "bg-[#83ba2e]" : "bg-green-800"}`}
+                        onClick={() => changeDifficulty(id)}>
             {name}
         </div>
     )
